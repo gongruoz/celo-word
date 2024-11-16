d13fd8de1324c144af1783ea591f5aec80c455a3
Hazel Ruozhen Gong <ruozhen7@gmail.com>
2024-11-16 17:10:15 +0800

    adding metamask

--- packages/react-app/providers/AppProvider.tsx
+++ packages/react-app/providers/AppProvider.tsx
@@ -23,7 +23,7 @@ const connectors = connectorsForWallets(
   ],
   {
     appName: 'Celo Composer',
-    projectId: process.env.WC_PROJECT_ID ?? '044601f65212332475a09bc14ceb3c34',
+    projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID ?? '044601f65212332475a09bc14ceb3c34',
   }
 );
 
